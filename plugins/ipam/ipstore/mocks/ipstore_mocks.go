646a68cc11286263c87c0b035edb56331fbbdee5
Peng Yin <penyin@amazon.com>
2017-05-18 22:01:26 +0000

    Added function for ipam to release ip by unique id

--- plugins/ipam/ipstore/mocks/ipstore_mocks.go
+++ plugins/ipam/ipstore/mocks/ipstore_mocks.go
@@ -104,6 +104,17 @@ func (_mr *_MockIPAllocatorRecorder) Release(arg0 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "Release", arg0)
 }
 
+func (_m *MockIPAllocator) ReleaseByID(_param0 string) (string, error) {
+	ret := _m.ctrl.Call(_m, "ReleaseByID", _param0)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (_mr *_MockIPAllocatorRecorder) ReleaseByID(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "ReleaseByID", arg0)
+}
+
 func (_m *MockIPAllocator) SetLastKnownIP(_param0 net.IP) {
 	_m.ctrl.Call(_m, "SetLastKnownIP", _param0)
 }
